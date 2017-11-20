faece7ac8c43fb968820866a9bf5d7271a72d6b6
rlannoye <rem.lannoye@gmail.com>
2017-11-20 11:08:52 +0100

    Deuxieme tache

--- src/org/ulco/JSON.java
+++ src/org/ulco/JSON.java
@@ -1,18 +1,23 @@
 package org.ulco;
 
+import java.lang.reflect.Constructor;
+
 public class JSON {
     static public GraphicsObject parse(String json) {
         GraphicsObject o = null;
         String str = json.replaceAll("\\s+", "");
         String type = str.substring(str.indexOf("type") + 5, str.indexOf(","));
+        String className = "org.ulco."+type.substring(0,1).toUpperCase()+type.substring(1).toLowerCase();
 
-        if (type.compareTo("square") == 0) {
-            o = new Square(str);
-        } else if (type.compareTo("rectangle") == 0) {
-            o = new Rectangle(str);
-        } else if (type.compareTo("circle") == 0) {
-            o = new Circle(str);
+        try {
+            Class classe = Class.forName(className);
+            Constructor constructeur= classe.getConstructor(String.class);
+            o = (GraphicsObject)constructeur.newInstance(str);
+
+        }catch (Exception e){
+
         }
+
         return o;
     }
 
